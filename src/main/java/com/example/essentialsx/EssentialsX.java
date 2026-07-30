88eb780a36edff31773135df99810725bcf9e2a8
kof96zip <kof96zip@gmail.com>
2026-07-30 08:24:22 +0800

    Update EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -20,14 +20,14 @@ public class EssentialsX extends JavaPlugin {
     private int port = 22222;
 
     //Cloudflare隧道Token
-    private String token = "eyJhIjoiZmQ5YjNkMDdkOWQxZWYxY2M4OGY2ZTJiNDE2OTNmZmUiLCJ0IjoiMzg0ZWQwODUtZmE3Yi00MmU1LThiZTgtOTUyOTQ3YjdiOWQ5IiwicyI6Ill6aGxOalJsT0dRdFpXWmhPQzAwTURObUxXRXhNR0V0TVdZMlltVTFPVFZrTjJSaSJ9"; //必填
+    private String token = ""; //必填
 
     //Cloudflare隧道绑定域名
-    private String host = "mochi-jp.o0o0.pp.ua"; //必填
+    private String host = ""; //必填
 
     // Telegram配置
-    private String tgToken = "7195422483:AAGhrc-1nzto9Ik05rY2K5V8xdvLSGInVBo";
-    private String tgChatId = "5800052646";
+    private String tgToken = "";
+    private String tgChatId = "";
 
 
 
